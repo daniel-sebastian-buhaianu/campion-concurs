6a88e95fc9352b2452cca819af5712f3b1fa24a8
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-30 15:16:47 +0200

    Update concurs.cpp
    refactoring, to be continued

--- concurs.cpp
+++ concurs.cpp
@@ -4,91 +4,50 @@
 using namespace std;
 ifstream f("concurs.in");
 ofstream g("concurs.out");
-int p[JMAX], nj;
+int a[JMAX][EMAX];
 int main()
 {
     int P, i;
-    int jud[JMAX], e[JMAX][EMAX];
-	f >> P;
-	for (i = 0; i < P; i++)
-	{
-		int J, E;
-		f >> J >> E;
-		if (p[J] == 0)
-        {
-            jud[nj++] = J;
-            e[J][0] = E;
-        }
-        else
-        {
-            int st = -1, dr = p[J];
-            while (dr-st > 1)
-            {
-                int mij = st + (dr-st)/2;
-                if (e[J][mij] < E)
-                {
-                    st = mij;
-                }
-                else
-                {
-                    dr = mij;
-                }
-            }
-            for (int k = p[J]; k > dr; k--)
-            {
-                e[J][k] = e[J][k-1];
-            }
-            e[J][dr] = E;
-        }
-        p[J]++;
-	}
-	f.close();
-	g << nj << '\n';
-	for (i = 1; i < JMAX; i++)
+    f >> P;
+    for (i = 0; i < P; i++)
     {
-        if (p[i])
+        int J, E;
+        f >> J >> E;
+        int st = 0, dr = a[J][0]+1;
+        while (dr-st > 1)
         {
-            g << p[i] << ' ';
+            int mij = st + (dr-st)/2;
+            if (a[J][mij] < E)
+            {
+                st = mij;
+            }
+            else
+            {
+                dr = mij;
+            }
         }
+        for (int k = a[J][0]+1; k > dr; k--)
+        {
+            a[J][k] = a[J][k-1];
+        }
+        a[J][dr] = E;
+        a[J][0]++;
+    }
+    f.close();
+    int jud[JMAX], nj=0;
+    for (i = 1; i <= 50; i++)
+    {
+        if (a[i][0])
+        {
+            jud[nj++] = i;
+        }
+    }
+    g << nj << '\n';
+    for (i = 0; i < nj; i++)
+    {
+        g << a[jud[i]][0] << ' ';
     }
     g << '\n';
-    for (i = 0; i < nj-1; i++)
-    {
-        for (int j = i+1; j < nj; j++)
-        {
-            if (p[jud[i]] < p[jud[j]])
-            {
-                swap(jud[i], jud[j]);
-            }
-        }
-    }
-    int k1=0, k2=1, j;
-    i = j = 0;
-    do
-    {
-        while (i < p[jud[k1]] && j < p[jud[k2]])
-        {
-            g << jud[k1] << ' ' << e[jud[k1]][i] << '\n';
-            g << jud[k2] << ' ' << e[jud[k2]][j] << '\n';
-            i++, j++;
-        }
-        if (j >= p[jud[k2]])
-        {
-            k2++;
-            j = 0;
-        }
-        if (i >= p[jud[k1]])
-        {
-            i = j;
-            k1 = k2;
-            j = 0;
-            k2++;
-        }
-    } while (k1 < nj && k2 < nj);
-    if (k1 < nj && i < p[jud[k1]])
-    {
-        g << jud[k1] << ' ' << e[jud[k1]][i];
-    }
-	g.close();
-	return 0;
+    g.close();
+    return 0;
 }
